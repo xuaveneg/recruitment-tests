51394fa0b72df62f7dc032bdb19930e5668391a8
Xuaveneg <genevaub@xinra.net>
2020-01-27 22:32:15 +0100

    Page d'accueil

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Home() {
     return (
-        <div />
+        <div>
+            <div>
+                Bienvenue sur le site de la Bibliothèque d'Henri Potier.
+            </div>
+            <div>
+                Vous pourrez ici avoir <Link to='/library'>l'inventaire</Link> des livres de la bibliothèque
+                et les ajouter à <Link to='/basket'>votre panier</Link>.
+            </div>
+        </div>
     );
 }
 
---
+++ src/views/Home.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {shallow, render} from 'enzyme';
+import {Link} from 'react-router-dom';
+
+import Home from './Home';
+
+test('should have 2 divs', () => {
+    // GIVEN
+
+    // WHEN
+    const home = shallow(<Home />);
+
+    // THEN
+    expect(home.find('div')).toHaveLength(3);
+});
+
+test('should have 2 correct links', () => {
+    // GIVEN
+
+    // WHEN
+    const home = shallow(<Home />);
+
+    // THEN
+    expect(home.find(Link)).toHaveLength(2);
+    expect(home.find(Link).get(0).props).toHaveProperty('to', '/library');
+    expect(home.find(Link).get(0).props).toHaveProperty('to', '/basket');
+});
