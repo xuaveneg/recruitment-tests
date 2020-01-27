b32714642539b9ab1809d8f2d345888385d4f680
Xuaveneg <genevaub@xinra.net>
2020-01-27 22:04:56 +0100

    Finalisation du layout détaillé

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 function Nav() {
     return (
-        <div>
-
-        </div>
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/library">Inventaire</Link>
+                    </li>
+                    <li>
+                        <Link to="/basket">Panier</Link>
+                    </li>
+                </ul>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/layout/Nav.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+
+import Nav from './Nav';
+
+test('should render 3 correct links', () => {
+    // GIVEN
+
+    // WHEN
+    const nav = shallow(<Nav />);
+
+    // THEN
+    expect(nav.find(Link)).toHaveLength(3);
+    expect(nav.find(Link).get(0).props).toHaveProperty('to', '/');
+    expect(nav.find(Link).get(1).props).toHaveProperty('to', '/library');
+    expect(nav.find(Link).get(2).props).toHaveProperty('to', '/basket');
+});
+
+test('should have Router', () => {
+    // GIVEN
+
+    // WHEN
+    const nav = shallow(<Nav />);
+
+    // THEN
+    expect(nav.is(Router)).toBe(true);
+});
+
+test('should have single div', () => {
+    // GIVEN
+
+    // WHEN
+    const nav = shallow(<Nav />);
+
+    // THEN
+    expect(nav.find('div')).toHaveLength(1);
+});
+
+test('should have single ul', () => {
+    // GIVEN
+
+    // WHEN
+    const nav = shallow(<Nav />);
+
+    // THEN
+    expect(nav.find('ul')).toHaveLength(1);
+});
+
+test('should have 3 li', () => {
+    // GIVEN
+
+    // WHEN
+    const nav = shallow(<Nav />);
+
+    // THEN
+    expect(nav.find('li')).toHaveLength(3);
+});
